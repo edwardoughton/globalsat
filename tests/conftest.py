d3c4ebf3364448b7db1dd10a4809217a050c7ce1
ej550 <edward.oughton@gmail.com>
2020-12-30 13:45:52 +0000

    Add tests

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+# from pytest import fixture
+
+
+# @fixture(scope='function')
+# def setup_region():
+#     return [{
+#     'GID_0': 'MWI',
+#     'GID_id': 'MWI.1.1.1_1',
+#     'mean_luminosity_km2': 26.736407691655717,
+#     'population': 10000,
+#     'area_km2': 2,
+#     'population_km2': 5000,
+#     'decile': 100,
+#     'geotype': 'urban',
+#     'demand_mbps_km2': 5000,
+#     'sites_estimated_km2': 2
+#     }]
