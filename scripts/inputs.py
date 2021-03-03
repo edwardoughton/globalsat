a77c0f9fd0295b34ffa8ae3b2a7eb25f3dca13ec
edwardoughton <edward.oughton@gmail.com>
2021-03-03 18:11:30 +0000

    Update inputs

--- scripts/inputs.py
+++ scripts/inputs.py
@@ -10,7 +10,7 @@ December 2020
 parameters = {
     'starlink': {
         'number_of_satellites': 6000,
-        'iterations': 5,
+        'iterations': 10,
         'seed_value': 42,
         'mu': 1, #Mean of distribution
         'sigma': 7.8, #Standard deviation of distribution
@@ -24,16 +24,19 @@ parameters = {
         'antenna_efficiency': 0.6,
         'power': 30, #dBw
         'losses': 4, #dB
+        'receiver_gain': 38,
+        'rain_attenuation': 10, #Rain Attenuation
+        'all_other_losses': 0.53, #All other losses
     },
     'oneweb': {
         'number_of_satellites': 600,
-        'iterations': 5,
+        'iterations': 10,
         'seed_value': 42,
         'mu': 1, #Mean of distribution
         'sigma': 7.8, #Standard deviation of distribution
         'total_area_earth_km_sq': 510000000, #Area of Earth in km^2
         'portion_of_earth_covered': 0.8, #We assume the poles aren't covered
-        'altitude_km': 550, #Altitude of starlink satellites in km
+        'altitude_km': 1200, #Altitude of starlink satellites in km
         'dl_frequency': 13.5*10**9, #Downlink frequency in Hertz
         'dl_bandwidth': 0.25*10**9,
         'speed_of_light': 3.0*10**8, #Speed of light in vacuum
@@ -41,24 +44,46 @@ parameters = {
         'antenna_efficiency': 0.6,
         'power': 30, #dBw
         'losses': 4, #dB
+        'receiver_gain': 38,
+        'rain_attenuation': 10, #Rain Attenuation
+        'all_other_losses': 0.53, #All other losses
     },
-    'telesat': {
-        'number_of_satellites': 300,
-        'iterations': 5,
+    'kuiper': {
+        'number_of_satellites': 3236,
+        'iterations': 10,
         'seed_value': 42,
         'mu': 1, #Mean of distribution
         'sigma': 7.8, #Standard deviation of distribution
         'total_area_earth_km_sq': 510000000, #Area of Earth in km^2
         'portion_of_earth_covered': 0.8, #We assume the poles aren't covered
-        'altitude_km': 550, #Altitude of starlink satellites in km
-        'dl_frequency': 13.5*10**9, #Downlink frequency in Hertz
+        'altitude_km': 610, #Altitude of starlink satellites in km
+        'dl_frequency': 17.7*10**9, #Downlink frequency in Hertz
         'dl_bandwidth': 0.25*10**9,
         'speed_of_light': 3.0*10**8, #Speed of light in vacuum
         'antenna_diameter': 0.7, #Metres
         'antenna_efficiency': 0.6,
         'power': 30, #dBw
-        'losses': 4, #dB
+        'receiver_gain': 38,
+        'rain_attenuation': 10, #Rain Attenuation
+        'all_other_losses': 0.53, #All other losses
     },
+    # 'telesat': {
+    #     'number_of_satellites': 300,
+    #     'iterations': 5,
+    #     'seed_value': 42,
+    #     'mu': 1, #Mean of distribution
+    #     'sigma': 7.8, #Standard deviation of distribution
+    #     'total_area_earth_km_sq': 510000000, #Area of Earth in km^2
+    #     'portion_of_earth_covered': 0.8, #We assume the poles aren't covered
+    #     'altitude_km': 550, #Altitude of starlink satellites in km
+    #     'dl_frequency': 13.5*10**9, #Downlink frequency in Hertz
+    #     'dl_bandwidth': 0.25*10**9,
+    #     'speed_of_light': 3.0*10**8, #Speed of light in vacuum
+    #     'antenna_diameter': 0.7, #Metres
+    #     'antenna_efficiency': 0.6,
+    #     'power': 30, #dBw
+    #     'losses': 4, #dB
+    # },
 }
 
 lut = [
