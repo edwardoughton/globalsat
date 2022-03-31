5cb8ee770fec86124519c9f863789f724ae6d2cb
makavelli <edwardoughton@gmail.com>
2022-03-31 13:48:45 -0400

    computation of radius and area at different latitudes for a user on the earth surface

--- scripts/coverage.py
+++ scripts/coverage.py
@@ -1,1 +1,24 @@
+from aifc import Aifc_read
+from sunau import Au_read
 import numpy as np
+
+equator = 0                   #Reference point
+earth_radius_equator = 6378   #Radius of the earth at the equator in km
+earth_radius_poles = 6357     # Radius of the earth at the poles in km
+
+def coverage_area(latitude_interval):
+    coverage_data = []
+    for angles in range(0,90,latitude_interval): #np.sin(np.radians(60))
+        latitude2 = angles
+        numerator = (((earth_radius_equator**2)*(np.cos(np.radians(angles))))
+                     ** 2 + ((earth_radius_poles**2)*(np.sin(np.radians(angles))))**2)
+        denominator = ((earth_radius_poles*(np.cos(np.radians(angles))))
+                       ** 2)+((earth_radius_poles*(np.sin(np.radians(angles))))**2)
+        earth_radius = np.sqrt(numerator/denominator)
+        latitude_change = angles-equator
+        area = 2*np.pi*earth_radius*((np.sin(latitude2))-(np.sin(equator)))
+        coverage_data.append({'Equator':equator,'Instantenous angle': angles,
+                                'Instantenous coverage area':area})
+    print(coverage_data)
+    return coverage_data 
+coverage_area(10)
