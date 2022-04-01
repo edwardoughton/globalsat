199ee4f6d2336bc328f6b8a8a6c3250d4fcfe103
makavelli <edwardoughton@gmail.com>
2022-03-31 21:04:36 -0400

    coverage area

--- scripts/coverage.py
+++ scripts/coverage.py
@@ -8,7 +8,7 @@ earth_radius_poles = 6356.752     # Radius of the earth at the poles in km
 
 def coverage_area(latitude_interval):
     coverage_data = []
-    for angles in range(0, 91, latitude_interval):  # np.sin(np.radians(60))
+    for angles in range(0, 91, latitude_interval):  
         latitude2 = angles
         numerator = (((earth_radius_equator**2)*(np.cos(np.radians(angles))))
                      ** 2)+(((earth_radius_poles**2)*(np.sin(np.radians(angles))))**2)
@@ -16,12 +16,11 @@ def coverage_area(latitude_interval):
                         ** 2)+(((earth_radius_poles)*(np.sin(np.radians(angles))))**2))
         radius_at_latitude = np.sqrt(numerator/denominator)
         latitude_change = angles-equator
-        #area = 2*np.pi*radius_at_latitude*((np.sin(np.radians(latitude2)))-(np.sin(np.radians(equator))))
-        eccentricity = 1-((earth_radius_poles**2)/(radius_at_latitude**2))
-        area = 4*np.pi*radius_at_latitude**2
+        eccentricity = 1-((earth_radius_poles**2)/(radius_at_latitude**2)) #Compute eccentricity
+        area = 4*np.pi*radius_at_latitude**2  #Circular area at given latitude
         if eccentricity != 0:
             earth_surface_area_at_latitude = (2*radius_at_latitude**2)*(
-                1+(((1-(eccentricity**2))/eccentricity)*(mt.atanh(eccentricity))))
+                1+(((1-(eccentricity**2))/eccentricity)*(mt.atanh(eccentricity))))#surface area of the earth at given latitude
             coverage_data.append({'Equator': equator, 'User latitude': angles,
                                   'Radius_at_latitude': radius_at_latitude,
                                   'Area of a circle at the latitude':
