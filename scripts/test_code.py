0e1dd98aa5d48d14bc5792b70799b73c39bef9c4
makavelli <edwardoughton@gmail.com>
2022-04-08 11:03:02 -0400

    constellations

--- scripts/test_code.py
+++ scripts/test_code.py
@@ -1,6 +1,10 @@
 path = '/home/makavelli/Documents/GitHub Codes/'
 
 import coverage as cr
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import random
 import pandas as pd
 a = cr.coverage_area(1)
 df = pd.DataFrame(a)
